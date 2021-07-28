cf31d2746bab8d63e0cdca03117fcada6a1b948e
delta4v <vuhai1902@gmail.com>
2021-07-27 22:46:47 -0400

    landing

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,17 @@
 import { Fragment, FunctionComponent } from 'react';
+import './Home.scss';
+
 import Nav from '../nav/Nav';
+import { ReactComponent as BackGroundImg } from "../../assests/background.svg";
+import Container from "@material-ui/core/Container";
 
 const Home: FunctionComponent<any> = () => {
   return (
     <Fragment>
       <Nav/>
-      <div>Home</div>
+      <Container fixed className={'home-body'}>
+        <BackGroundImg width={400}/>
+      </Container>
     </Fragment>
   )
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import './Nav.scss';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,14 +10,19 @@ import { Typography } from '@material-ui/core';
 
 const Nav: FunctionComponent<any> = () => {
   return (
-    <AppBar color={'warning'}>
+    <AppBar className={'nav'} color={'warning'} position={'static'}>
       <Toolbar>
         <Button
+          style={{ display: 'grid' }}
+          className={'app-icon-btn'}
           variant={'text'}
-          color={'info'}
-          startIcon={<AppIcon width={40} height={40}/>}>
-          <Typography variant={'h5'}>
-            Nav
+          color={'info'}>
+          <AppIcon className={'app-icon'} width={40} height={40}/>
+          <Typography className={'app-name-1'} variant={'subtitle1'}>
+            Path
+          </Typography>
+          <Typography className={'app-name-2'} variant={'subtitle1'}>
+            Finder
           </Typography>
         </Button>
       </Toolbar>
